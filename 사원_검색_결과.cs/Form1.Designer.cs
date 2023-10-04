d75f739a1e9ca8e813b40d3f93edc18715a6f7c4
Lee_jinhyeok <121598818+jinhuck854@users.noreply.github.com>
2023-10-04 18:26:12 +0900

    Add files via upload
    Connection DataBase - Visual Studio
    And Practice Example.

---
+++ 사원_검색_결과.cs/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace 사원_검색_결과
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Oracle.ManagedDataAccess.Client.OracleParameter oracleParameter3 = new Oracle.ManagedDataAccess.Client.OracleParameter();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.oracleConnection1 = new Oracle.ManagedDataAccess.Client.OracleConnection();
+            this.oracleCommand1 = new Oracle.ManagedDataAccess.Client.OracleCommand();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(514, 150);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "사원 검색";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(354, 152);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(354, 208);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 21);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(354, 263);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 21);
+            this.textBox3.TabIndex = 3;
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox4.Location = new System.Drawing.Point(269, 155);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(58, 14);
+            this.textBox4.TabIndex = 4;
+            this.textBox4.Text = "EMPNO";
+            // 
+            // textBox5
+            // 
+            this.textBox5.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.textBox5.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox5.Location = new System.Drawing.Point(269, 211);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(58, 14);
+            this.textBox5.TabIndex = 5;
+            this.textBox5.Text = "ENAME";
+            // 
+            // textBox6
+            // 
+            this.textBox6.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.textBox6.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox6.Location = new System.Drawing.Point(269, 266);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(35, 14);
+            this.textBox6.TabIndex = 6;
+            this.textBox6.Text = "JOB";
+            // 
+            // oracleConnection1
+            // 
+            this.oracleConnection1.ChunkMigrationConnectionTimeout = "120";
+            // 
+            // oracleCommand1
+            // 
+            this.oracleCommand1.CommandText = "SELECT * FROM EMP WHERE EMPNO=:PARAM";
+            oracleParameter3.OracleDbType = Oracle.ManagedDataAccess.Client.OracleDbType.Varchar2;
+            oracleParameter3.ParameterName = "pa";
+            this.oracleCommand1.Parameters.Add(oracleParameter3);
+            this.oracleCommand1.Transaction = null;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(514, 208);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "연결끊기";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private Oracle.ManagedDataAccess.Client.OracleConnection oracleConnection1;
+        private Oracle.ManagedDataAccess.Client.OracleCommand oracleCommand1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ 사원_검색_결과.cs/Form1.cs
@@ -0,0 +1,65 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 사원_검색_결과
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string conString = "USER ID=C##5584980;PASSWORD=1234;Data source=localhost:1521/xepdb1";
+
+            // 도구에서 생성도 가능// OracleConnection oracleConnection1 = new OracleConnection();
+
+            oracleConnection1.ConnectionString = conString;
+            oracleConnection1.Open();
+            MessageBox.Show(oracleConnection1.State.ToString());
+            oracleCommand1.Connection = oracleConnection1;
+        }
+
+        private void button1_Click(object sender, EventArgs e) // 사원 검색 버튼
+        {
+            // oracleConnection1.Open();
+            // oracleCommand1.CommandText = "SELECT * FROM EMP WHERE EMPNO=:PARAM";
+
+            // oracleCommand1.Connection = oracleConnection1; // 이거 안 해서..? 하..
+
+            try
+            {
+                oracleCommand1.Parameters["pa"].Value = textBox1.Text; //Parameters[“pa”]로도 사용 가능
+                OracleDataReader rdr = oracleCommand1.ExecuteReader();
+                while (rdr.Read())
+                {
+                    textBox2.Text = rdr["ENAME"].ToString();
+                    textBox3.Text = rdr["JOB"].ToString();
+                }
+
+                rdr.Close();
+            }
+            catch
+            {
+                MessageBox.Show("No EMP");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            oracleConnection1.Close();
+            MessageBox.Show(oracleConnection1.State.ToString());
+            this.Close();
+        }
+    }
+}
