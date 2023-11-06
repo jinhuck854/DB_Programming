929704e57bff99155a47cc764423010c50846446
Lee_jinhyeok <121598818+jinhuck854@users.noreply.github.com>
2023-11-06 17:50:17 +0900

    Add files via upload
    Commit 메모장 프로그램

---
+++ 메모장.cs/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace 개인_메모장
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Oracle.ManagedDataAccess.Client.OracleParameter oracleParameter4 = new Oracle.ManagedDataAccess.Client.OracleParameter();
+            Oracle.ManagedDataAccess.Client.OracleParameter oracleParameter5 = new Oracle.ManagedDataAccess.Client.OracleParameter();
+            Oracle.ManagedDataAccess.Client.OracleParameter oracleParameter6 = new Oracle.ManagedDataAccess.Client.OracleParameter();
+            this.oracleConnection1 = new Oracle.ManagedDataAccess.Client.OracleConnection();
+            this.oracleCommand1 = new Oracle.ManagedDataAccess.Client.OracleCommand();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.oracleCommand2 = new Oracle.ManagedDataAccess.Client.OracleCommand();
+            this.oracleCommand3 = new Oracle.ManagedDataAccess.Client.OracleCommand();
+            this.oracleCommand4 = new Oracle.ManagedDataAccess.Client.OracleCommand();
+            this.oracleCommand5 = new Oracle.ManagedDataAccess.Client.OracleCommand();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // oracleConnection1
+            // 
+            this.oracleConnection1.ChunkMigrationConnectionTimeout = "120";
+            this.oracleConnection1.InfoMessage += new Oracle.ManagedDataAccess.Client.OracleInfoMessageEventHandler(this.oracleConnection1_InfoMessage);
+            // 
+            // oracleCommand1
+            // 
+            oracleParameter4.OracleDbType = Oracle.ManagedDataAccess.Client.OracleDbType.Varchar2;
+            oracleParameter4.ParameterName = "aa";
+            oracleParameter5.OracleDbType = Oracle.ManagedDataAccess.Client.OracleDbType.Date;
+            oracleParameter5.ParameterName = "bb";
+            oracleParameter6.OracleDbType = Oracle.ManagedDataAccess.Client.OracleDbType.Varchar2;
+            oracleParameter6.ParameterName = "cc";
+            this.oracleCommand1.Parameters.Add(oracleParameter4);
+            this.oracleCommand1.Parameters.Add(oracleParameter5);
+            this.oracleCommand1.Parameters.Add(oracleParameter6);
+            this.oracleCommand1.Transaction = null;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Appearance = System.Windows.Forms.TabAppearance.FlatButtons;
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(31, 31);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(496, 377);
+            this.tabControl1.SizeMode = System.Windows.Forms.TabSizeMode.Fixed;
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.richTextBox1);
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.textBox2);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(488, 348);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "메모 입력";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(35, 66);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(410, 263);
+            this.richTextBox1.TabIndex = 8;
+            this.richTextBox1.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(270, 25);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "다음 메모";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(189, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "메모 저장";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(94, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(89, 21);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.Info;
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(35, 27);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(53, 21);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "키워드";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.listBox2);
+            this.tabPage2.Controls.Add(this.button4);
+            this.tabPage2.Controls.Add(this.button3);
+            this.tabPage2.Controls.Add(this.listBox1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(488, 348);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "메모 리스트";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 12;
+            this.listBox2.Location = new System.Drawing.Point(26, 128);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(367, 172);
+            this.listBox2.TabIndex = 3;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(399, 71);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(83, 23);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "내용 보기";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(399, 18);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(83, 23);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "리스트 보기";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(26, 18);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(367, 76);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // oracleCommand2
+            // 
+            this.oracleCommand2.Transaction = null;
+            // 
+            // oracleCommand3
+            // 
+            this.oracleCommand3.Transaction = null;
+            // 
+            // oracleCommand4
+            // 
+            this.oracleCommand4.Transaction = null;
+            // 
+            // oracleCommand5
+            // 
+            this.oracleCommand5.CommandText = "SELECT SYSDATE FROM DUAL";
+            this.oracleCommand5.Transaction = null;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(568, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Oracle.ManagedDataAccess.Client.OracleConnection oracleConnection1;
+        private Oracle.ManagedDataAccess.Client.OracleCommand oracleCommand1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private Oracle.ManagedDataAccess.Client.OracleCommand oracleCommand2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ListBox listBox2;
+        private Oracle.ManagedDataAccess.Client.OracleCommand oracleCommand3;
+        private Oracle.ManagedDataAccess.Client.OracleCommand oracleCommand4;
+        private Oracle.ManagedDataAccess.Client.OracleCommand oracleCommand5;
+    }
+}
+
---
+++ 메모장.cs/Form1.cs
@@ -0,0 +1,140 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 개인_메모장
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string conString = "USER ID=C##5584980;PASSWORD=1234;Data source=localhost:1521/xepdb1";
+            oracleConnection1.ConnectionString = conString;
+            oracleConnection1.Open();
+            MessageBox.Show(oracleConnection1.State.ToString());
+            oracleCommand1.Connection = oracleConnection1; // 메모 저장
+            oracleCommand2.Connection = oracleConnection1; // 다음 메모 : 실제로는 안 씀
+            oracleCommand3.Connection = oracleConnection1; // 리스트 보기
+            oracleCommand4.Connection = oracleConnection1; // 내용 보기
+            oracleCommand5.Connection = oracleConnection1; // 시간 가져오기 ?
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e) // 메모 저장
+        {
+            DateTime current_time;
+            current_time = Convert.ToDateTime(oracleCommand5.ExecuteScalar());
+
+            oracleCommand1.CommandText = "INSERT INTO MEMO_TABLE (M_ID, M_KEYWORD, M_DATE, M_CONTENTS) VALUES(memo_seq.nextval, :aa, :bb, :cc)";
+            //INSERT INTO MEMO_TABLE(M_ID,M_KEYWORD,M_DATE,M_CONTENTS) VALUES(memo_seq.nextval, :aa, :bb, :cc)
+
+            oracleCommand1.Parameters["aa"].Value = textBox1.Text;
+            oracleCommand1.Parameters["bb"].Value = current_time;
+            oracleCommand1.Parameters["cc"].Value = richTextBox1.Text;
+
+            oracleCommand1.ExecuteNonQuery(); //  입력 구문이므로 Commit
+            MessageBox.Show("Memo successfully added");
+
+
+            /*
+             * (case 1)
+             * oracleCommand1.Parameters["aa"].Value = textBox1.Text;
+             * oracleCommand1.Parameters["bb"].Value = current_time.ToString();
+             * oracleCommand1.Parameters["cc"].Value = richTextBox1.Text;
+             * 
+             * (case 2)
+             * oracleCommand1.Parameters.Add("bb", OracleDbType.Date, current_time, ParameterDirection.Input); // 현재 시간
+             * 
+             * (case 3)
+             * oracleCommand1.Parameters.Add(new OracleParameter("aa", textBox1.Text));
+             * oracleCommand1.Parameters.Add(new OracleParameter("bb", current_time));
+             * oracleCommand1.Parameters.Add(new OracleParameter("cc", richTextBox1.Text));
+             */
+        }
+
+        private void button2_Click(object sender, EventArgs e) // 다음 메모
+        {
+            // 메모지 초기화
+            textBox1.Text = null; // ""
+            richTextBox1.Text = null; // ""
+        }
+
+        private void button3_Click(object sender, EventArgs e) // 메모 리스트 보기
+        {
+            oracleCommand3.CommandText = "SELECT M_ID FROM MEMO_TABLE";
+            OracleDataReader rdr = oracleCommand3.ExecuteReader();
+            while (rdr.Read())
+            {
+                listBox1.Items.Add(rdr["M_ID"]);
+            }
+            rdr.Close();
+        }
+
+        private void button4_Click(object sender, EventArgs e) // 내용 보기
+        {
+            try
+            {
+                string sql = "SELECT M_DATE, M_CONTENTS FROM MEMO_TABLE WHERE M_id=";
+                string num = listBox1.SelectedItem.ToString(); // ?
+                oracleCommand4.CommandText = sql + num;
+                OracleDataReader rdr = oracleCommand4.ExecuteReader();
+
+                listBox2.Items.Clear();
+                int count = rdr.FieldCount;
+
+                while (rdr.Read())
+                {
+                    listBox2.Items.Add(rdr["M_DATE"]);
+                    listBox2.Items.Add(rdr["M_CONTENTS"]);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Select Memo Plz");
+            }
+            
+
+        }
+
+        private void oracleConnection1_InfoMessage(object sender, OracleInfoMessageEventArgs eventArgs)
+        {
+            string conString = "USER ID=C##5584980;PASSWORD=1234;Data source=localhost:1521/xepdb1";
+            oracleConnection1.ConnectionString = conString;
+            oracleConnection1.Open();
+            MessageBox.Show(oracleConnection1.State.ToString());
+            oracleCommand1.Connection = oracleConnection1; // 오류 원인
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e) // 메모리스트
+        {
+
+        }
+
+        
+
+        
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e) // 메모의
+        {
+
+        }
+
+        
+    }
+}
